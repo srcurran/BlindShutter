f5fc22551cc2daafbefa419152c255ecc7f4b8c7
Sean Curran <srcurran@gmail.com>
2025-02-28 21:38:22 -0500

    restructured to work with capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.srcurran.bsapp',
+  appName: 'Blind Shutter',
+  webDir: 'dist'
+};
+
+export default config;
